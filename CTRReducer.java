ff16b6ce0d878d54580435cff5818458c8c34095
yujinlee0130 <yujinlee0130@gmail.com>
2024-03-14 12:33:17 -0400

    update

--- CTRReducer.java
+++ CTRReducer.java
@@ -2,7 +2,9 @@ import java.io.IOException;
 
 import javax.naming.Context;
 
-import org.w3c.dom.Text;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
 
 public class CTRReducer extends Reducer<Text, Text, Text, Text>{
 @Override 
